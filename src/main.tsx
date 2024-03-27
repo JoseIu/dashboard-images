a4db85491ddf63b5449d9293a3bfd036c9ee9387
Joselu <joseluissgz97@gmail.com>
2024-03-27 17:51:36 +0100

    initial config

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.scss';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <App />,
+  // <React.StrictMode>
+  // </React.StrictMode>,
+);
