a4b71cc783d8603a97ae77d10ac61a08eca00a6d
Joselu <joseluissgz97@gmail.com>
2024-03-27 17:48:38 +0100

    add: Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import Navigation from '../../routes/Navigation';
+import style from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={`${style.header} wrapper`}>
+      <h1 className={style.header__logo}>Logo</h1>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
