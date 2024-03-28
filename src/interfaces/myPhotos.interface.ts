e2fd109ce5399d51abbd05dd83ff056bd007b9bc
Joselu <joseluissgz97@gmail.com>
2024-03-28 10:20:04 +0100

    add: MyPhoyos interface

---
+++ src/interfaces/myPhotos.interface.ts
@@ -0,0 +1,10 @@
+export interface MyPhoyos {
+  id: string;
+  image: string;
+  width: number;
+  height: number;
+  likes: number;
+  description: string | null;
+  alt_description: string;
+  created_at: string;
+}
