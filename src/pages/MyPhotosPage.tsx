dd439f913fb40b421b1939b70b1d63dcb3e385cd
Joselu <joseluissgz97@gmail.com>
2024-03-28 17:37:06 +0100

    feat: MyPhotosPage

--- src/pages/MyPhotosPage.tsx
+++ src/pages/MyPhotosPage.tsx
@@ -1,5 +1,17 @@
+import { useSelector } from 'react-redux';
+import { RooState } from '../app/store';
+import ImagesSavedList from '../components/ImagesSavedList/ImagesSavedList';
+
 const MyPhotosPage = () => {
-  return <div>MyPhotosPage</div>;
+  const imagesSaved = useSelector((state: RooState) => state.myPhoto);
+  // console.log(imagesSaved);
+
+  return (
+    <div>
+      MyPhotosPage
+      <ImagesSavedList images={imagesSaved} isMyPhotosPage={true} />
+    </div>
+  );
 };
 
 export default MyPhotosPage;
