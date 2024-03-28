ca1e7e24699e86b98140e0b895e1849b49eb9212
Joselu <joseluissgz97@gmail.com>
2024-03-28 10:19:00 +0100

    add: Provider store

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App.tsx';
+import { store } from './app/store.ts';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   // <React.StrictMode>
   // </React.StrictMode>,
 );
