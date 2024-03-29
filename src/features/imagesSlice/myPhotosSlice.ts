b069b54853f6ca4ee576e32171f85e4ba4784190
Joselu <joseluissgz97@gmail.com>
2024-03-29 21:57:23 +0100

    feat: selectedImageto edit image description and addImageFromStorage

--- src/features/imagesSlice/myPhotosSlice.ts
+++ src/features/imagesSlice/myPhotosSlice.ts
@@ -1,20 +1,45 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { MyPhoTos } from '../../interfaces/myPhotos.interface';
-const initialState: MyPhoTos[] = [];
+
+interface SavedImageState {
+  myPhotos: MyPhoTos[];
+  selectedImage: string | null;
+}
+// const initialState: MyPhoTos[] = [];
+const initialState: SavedImageState = {
+  myPhotos: [],
+  selectedImage: null,
+};
 
 const myPhotosSlice = createSlice({
   name: 'myPhotos',
   initialState,
   reducers: {
     addImage: (state, action) => {
-      return [...state, action.payload];
+      console.log(action.payload);
+      state.myPhotos.push(action.payload);
+    },
+    addImageFromStorage: (state, action) => {
+      state.myPhotos = action.payload;
     },
     deleteImage: (state, action) => {
-      return state.filter((image) => image.id !== action.payload);
+      state.myPhotos = state.myPhotos.filter((image) => image.id !== action.payload);
+    },
+    editImage: (state, action) => {
+      const { id, edit } = action.payload;
+      const finIndex = state.myPhotos.findIndex((image) => image.id === id);
+
+      if (finIndex !== -1) {
+        state.myPhotos[finIndex].description = edit;
+      }
+    },
+    selectImageById: (state, action) => {
+      state.selectedImage = action.payload;
     },
   },
 });
 
-export const { deleteImage, addImage } = myPhotosSlice.actions;
+export const { deleteImage, addImage, selectImageById, editImage, addImageFromStorage } =
+  myPhotosSlice.actions;
 
 export default myPhotosSlice.reducer;
