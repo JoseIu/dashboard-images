c8cf52f0c79336e08dbfbce1cc8ed6dcc5e39ada
Joselu <joseluissgz97@gmail.com>
2024-03-29 21:59:05 +0100

    add: feat to how form to edit and set imagesSaved from Storage

--- src/pages/MyPhotosPage.tsx
+++ src/pages/MyPhotosPage.tsx
@@ -1,16 +1,62 @@
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { RooState } from '../app/store';
+import EditForm from '../components/EditForm/EditForm';
 import ImagesSavedList from '../components/ImagesSavedList/ImagesSavedList';
+import InputSearch from '../components/InputSearch/InputSearch';
+import { addImageFromStorage } from '../features/imagesSlice/myPhotosSlice';
+import style from './MyPhotosPage.module.scss';
 
 const MyPhotosPage = () => {
-  const imagesSaved = useSelector((state: RooState) => state.myPhoto);
-  // console.log(imagesSaved);
+  const [isEditing, setIsEditing] = useState(false);
+  const dispatch = useDispatch();
+
+  const imagesSaved = useSelector((state: RooState) => state.myPhoto.myPhotos);
+  const imageToEditId = useSelector((state: RooState) => state.myPhoto.selectedImage);
+
+  console.log({ imagesSaved });
+
+  useEffect(() => {
+    const imagesFromStorage = JSON.parse(localStorage.getItem('savedImages') || '[]');
+
+    if (!imagesFromStorage.length) return;
+    dispatch(addImageFromStorage(imagesFromStorage));
+    console.log({ imagesFromStorage });
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!imageToEditId || imageToEditId === null) return;
+
+    setIsEditing(true);
+  }, [imageToEditId]);
 
   return (
-    <div>
-      MyPhotosPage
+    <section className={`${style.saved} wrapper`}>
+      <h2>MyPhotosPage</h2>
+      {isEditing ? (
+        <EditForm id={imageToEditId} setIsEditing={setIsEditing} />
+      ) : (
+        <div className={style.filters}>
+          <InputSearch placeholder="Search an image..." />
+
+          <select name="select" id="selet" className={style.select}>
+            <option value="0">Default</option>
+            <option value="0">Default</option>
+            <option value="0">Default</option>
+            <option value="0">Default</option>
+            <option value="0">Default</option>
+          </select>
+
+          <ul>
+            <li>a</li>
+            <li>a</li>
+            <li>a</li>
+            <li>a</li>
+          </ul>
+        </div>
+      )}
       <ImagesSavedList images={imagesSaved} isMyPhotosPage={true} />
-    </div>
+    </section>
   );
 };
 
