fc601263677596c5280fe4ff343abd47a88790cd
Joselu <joseluissgz97@gmail.com>
2024-04-03 22:41:35 +0200

    removing console.log

--- src/helpers/editFromStorage.ts
+++ src/helpers/editFromStorage.ts
@@ -1,7 +1,6 @@
 import { MyPhoTos } from '../interfaces/myPhotos.interface';
 
 const editFromStorage = (id: string | null, data: string) => {
-  console.log('DENTRO');
   const imagesSaved: MyPhoTos[] = JSON.parse(localStorage.getItem('savedImages') || '[]');
 
   const findImage = imagesSaved.findIndex((image) => image.id === id);
