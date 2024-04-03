8260c9cee12ab9fe683364607aa925429cbcc470
Joselu <joseluissgz97@gmail.com>
2024-04-03 12:31:02 +0200

    fix: fixing codition if findImage === -1

--- src/helpers/editFromStorage.ts
+++ src/helpers/editFromStorage.ts
@@ -1,11 +1,12 @@
 import { MyPhoTos } from '../interfaces/myPhotos.interface';
 
 const editFromStorage = (id: string | null, data: string) => {
+  console.log('DENTRO');
   const imagesSaved: MyPhoTos[] = JSON.parse(localStorage.getItem('savedImages') || '[]');
 
   const findImage = imagesSaved.findIndex((image) => image.id === id);
 
-  if (!findImage) return;
+  if (findImage === -1) return;
 
   imagesSaved[findImage].description = data;
 
