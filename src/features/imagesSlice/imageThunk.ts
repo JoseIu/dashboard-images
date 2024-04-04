d2a0e9e02c2ed180b9d7ee055ffead36b9ca725a
Joselu <joseluissgz97@gmail.com>
2024-04-04 12:12:27 +0200

    feart: getImagesThunk

---
+++ src/features/imagesSlice/imageThunk.ts
@@ -0,0 +1,12 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { Result } from '../../interfaces/Result.interface';
+
+const BASE_URL = import.meta.env.VITE_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const getImages = createAsyncThunk('images/get', async (search: string) => {
+  const response = await fetch(`${BASE_URL}/?query=${search}&client_id=${API_KEY}`);
+  const data: Result = (await response.json()) as Result;
+  console.log(data);
+  return data;
+});
