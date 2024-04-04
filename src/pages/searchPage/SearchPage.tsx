c9426e4534bd695b4231c60f916574c524e576a8
Joselu <joseluissgz97@gmail.com>
2024-04-04 12:15:59 +0200

    add: dispatch to getImages thunk

--- src/pages/searchPage/SearchPage.tsx
+++ src/pages/searchPage/SearchPage.tsx
@@ -1,29 +1,21 @@
 import { FormEvent, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { RooState } from '../../app/store';
+import { AppDispatch, RooState } from '../../app/store';
 import ImagesList from '../../components/ImagesList/ImagesList';
 import InputSearch from '../../components/InputSearch/InputSearch';
+import { getImages } from '../../features/imagesSlice/imageThunk';
 import style from './SearchPage.module.scss';
 
 const SearchPage = () => {
   const [search, setSearch] = useState('');
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
 
   const { images } = useSelector((state: RooState) => state.images);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const BASE_URL = import.meta.env.VITE_URL;
-    const API_KEY = import.meta.env.VITE_API_KEY;
-
-    try {
-      // const response = await fetch(`${BASE_URL}/?query=${search}&client_id=${API_KEY}`);
-      // const data = (await response.json()) as Result;
-      // dispatch(addResult(data.results));
-    } catch (error) {
-      console.log(error);
-    }
+    dispatch(getImages(search));
   };
 
   return (
