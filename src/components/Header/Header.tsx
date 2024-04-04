549295a8f3fbfc0cac664ab5aca04c76cc9b45a0
Joselu <joseluissgz97@gmail.com>
2024-04-04 16:52:53 +0200

    fix: fixing header structure and styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,8 +4,10 @@ import style from './Header.module.scss';
 const Header = () => {
   return (
     <header className={`${style.header} wrapper`}>
-      <h1 className={style.header__logo}>Logo</h1>
-      <Navigation />
+      <div className={style.header__container}>
+        <h1 className={style.header__logo}>Logo</h1>
+        <Navigation />
+      </div>
     </header>
   );
 };
