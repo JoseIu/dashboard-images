accbad327791f95a401a4a16b711cf320202dad5
Joselu <joseluissgz97@gmail.com>
2024-04-04 12:42:39 +0200

    add: Loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,8 @@
+import style from './Loader.module.scss';
+const Loader = () => (
+  <div className={style.container}>
+    <div className={style.loader}></div>
+  </div>
+);
+
+export default Loader;
