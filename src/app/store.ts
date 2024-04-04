626f30ffd759dee27805214234995bf7b73770f6
Joselu <joseluissgz97@gmail.com>
2024-04-04 12:10:35 +0200

    add: AppThunk type

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit';
 import { imagesSlice } from '../features/imagesSlice/imagesSlice';
-import myPhotosReducer from '../features/imagesSlice/myPhotosSlice';
+import myPhotosReducer from '../features/myPhotosSlice/myPhotosSlice';
 
 export const store = configureStore({
   reducer: {
@@ -12,3 +12,4 @@ export const store = configureStore({
 export type RooState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RooState, unknown, Action<string>>;
