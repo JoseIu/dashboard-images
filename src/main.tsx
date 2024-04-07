98787f09f9cf8ebf54e196e7b75ef4c1cefc8ecc
Joselu <joseluissgz97@gmail.com>
2024-04-07 12:06:57 +0200

    React.StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
@@ -5,9 +6,10 @@ import { store } from './app/store.ts';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  // <React.StrictMode>
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    ,
+  </React.StrictMode>,
 );
