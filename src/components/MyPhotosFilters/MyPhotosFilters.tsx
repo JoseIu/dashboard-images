bc8bfaabcdf71bb23065b59e8ce63153449f9b62
Joselu <joseluissgz97@gmail.com>
2024-04-07 11:30:02 +0200

    fix: removing searchByTag and adding arrow for select

--- src/components/MyPhotosFilters/MyPhotosFilters.tsx
+++ src/components/MyPhotosFilters/MyPhotosFilters.tsx
@@ -1,4 +1,5 @@
 import InputSearch from '../InputSearch/InputSearch';
+import ArrowIcon from '../icons/ArrowIcon';
 import style from './MyPhotosFilters.module.scss';
 
 interface MyPhotosFilersProps {
@@ -6,48 +7,43 @@ interface MyPhotosFilersProps {
   setSearchByDescription: (search: string) => void;
   setSort: (sort: number) => void;
   sort: number;
-  setSearchByTag: (search: string) => void;
 }
 
-const MyPhotosFilers = ({
-  search,
-  setSearchByDescription,
-  sort,
-  setSort,
-  setSearchByTag,
-}: MyPhotosFilersProps) => {
+const MyPhotosFilers = ({ search, setSearchByDescription, sort, setSort }: MyPhotosFilersProps) => {
   return (
     <div className={style.filters}>
       <InputSearch
         value={search}
         className={style.filters__search}
-        placeholder="Search an image..."
+        placeholder="Filter by description..."
         onChange={(event) => setSearchByDescription(event.target.value)}
       />
-
-      <select
-        className={style.filters__select}
-        value={sort}
-        name="select"
-        id="selet"
-        onChange={(event) => setSort(Number(event.target.value))}
-      >
-        <option className={style.filters__option} value="0">
-          Default
-        </option>
-        <option className={style.filters__option} value="1">
-          Import date
-        </option>
-        <option className={style.filters__option} value="2">
-          Width
-        </option>
-        <option className={style.filters__option} value="3">
-          Height
-        </option>
-        <option className={style.filters__option} value="4">
-          Likes
-        </option>
-      </select>
+      <div className={style.filters__container}>
+        <select
+          value={sort}
+          name="select"
+          id="selet"
+          className={style.filters__select}
+          onChange={(event) => setSort(Number(event.target.value))}
+        >
+          <option className={style.filters__option} value="0">
+            Default
+          </option>
+          <option className={style.filters__option} value="1">
+            Import date
+          </option>
+          <option className={style.filters__option} value="2">
+            Width
+          </option>
+          <option className={style.filters__option} value="3">
+            Height
+          </option>
+          <option className={style.filters__option} value="4">
+            Likes
+          </option>
+        </select>
+        <ArrowIcon className={style.filters__arow} />
+      </div>
     </div>
   );
 };
